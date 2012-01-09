d1c110a2783f5e3adc414224ea944b64945f5cea
Roman Gonzalez <romanandreg@gmail.com>
2012-01-08 16:34:40 -0800

    Adding more highlights to error messages for koans

--- test/ParserCombinators.hs
+++ test/ParserCombinators.hs
@@ -6,17 +6,24 @@ import Test.Framework.Providers.HUnit (testCase)
 import Test.HUnit (assertBool, assertEqual)
 import qualified Data.Attoparsec.Text as P
 
-put_parser_here = undefined
-
 tests :: [Test]
 tests = 
     [
       testDigitParser
     ]
 
+--failParser :: P.Parser ()
+failParser parserName = 
+  fail $ "\n\n\t\x1B[32;1mCheck documentation\x1B[0m of \x1B[33;1m" 
+         ++ parserName 
+         ++ "\x1B[0m on:\n\t" 
+         ++ "http://hackage.haskell.org/packages/archive/attoparsec/0.10.1.0/doc/html/Data-Attoparsec-Text.html"
+
 testDigitParser :: Test
 testDigitParser = testCase "digit parser" $ do
-    let result = P.parseOnly put_parser_here "5"
+    -- Change parser with the correct parser to use
+    let parser = failParser "digit parser"
+    let result = P.parseOnly parser "5"
     case result of
       Left e -> assertBool e False
-      Right answer -> assertEqual "wrong parser" "5" answer
+      Right answer -> assertEqual "wrong parser" '5' answer
